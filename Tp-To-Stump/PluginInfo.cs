ca1244973d40f21814873a693328711863c9ce4e
Kirt <87604221+Kirnt@users.noreply.github.com>
2023-05-30 10:22:36 +0100

    Add files via upload

---
+++ Tp-To-Stump/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Tp_To_Stump
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.max.gorillatag.tp_to_stump";
+        public const string Name = "Tp_To_Stump";
+        public const string Version = "1.0.0";
+    }
+}
